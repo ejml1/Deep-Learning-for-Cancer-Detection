ba1f4169bc13c804fbdaa3792bdd194b7097811f
ejml1 <ejml1@st-andrews.ac.uk>
2024-01-22 13:56:55 +0000

    First submission draft

---
+++ Verify.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import imghdr
+
+def verify_image(image_path):
+    try:
+        img = cv2.imread(image_path)
+        tip = imghdr.what(image_path)
+    except Exception as e:
+        print('Error: ' + str(e))
+
+#data_dir = 'BM_cytomorphology_data'
+data_dir = 'Data_Subset'
+image_exts = ['jpg']
+
+for image_class in os.listdir(data_dir):
+    class_path = os.path.join(data_dir, image_class)
+    if os.path.isdir(class_path):
+        for maybe_image in os.listdir(class_path):
+            if os.path.isdir(os.path.join(data_dir, image_class, maybe_image)):
+                for image in os.listdir(os.path.join(data_dir, image_class, maybe_image)):
+                    image_path = os.path.join(data_dir, image_class, maybe_image, image)
+                    verify_image(image_path)
+            else:
+                image_path = os.path.join(data_dir, image_class, maybe_image)
+                verify_image(image_path)
